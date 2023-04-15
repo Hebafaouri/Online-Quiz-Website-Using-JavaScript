d74be3d2e5d15cbe91ea79d34734b85b41008bcd
Hebafaouri <126398763+Hebafaouri@users.noreply.github.com>
2023-04-15 16:47:08 +0300

    update data

--- Heba Faouri/login.js
+++ Heba Faouri/login.js
@@ -9,3 +9,52 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+// validation
+var nameError = document.getElementById('name-error');
+var emailError = document.getElementById('email-error');
+var passwordError = document.getElementById('password-error');
+var password2Error = document.getElementById('password2-error');
+
+
+function validateName(){ 
+  var name = document.getElementById('contact-name').value;
+  if (name.length==0){
+      nameError.innerHTML = 'Name is required'
+      return false;
+  }
+  if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+      nameError.innerHTML = 'Write full name'
+      return false;
+  }
+  nameError.innerHTML = '<i class="fa-solid fa-check"></i>';
+  return true;
+}
+function validateEmail(){
+  var email = document.getElementById('contact-email').value;
+
+  if(!email.match (/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+      emailError.innerHTML = 'Emai Invalid'
+      return false;
+  }
+  emailError.innerHTML= '<i class="fa-solid fa-check"></i>';
+  return true;  
+}
+function validatePassword(){
+  var password = document.getElementById('contact-password').value;
+  if(!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)){
+      passwordError.innerHTML = 'Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special characte'
+      return false;
+  }
+  passwordError.innerHTML= '<i class="fa-solid fa-check"></i>';
+  return true; 
+}
+function validatePassword2 (){
+  var password = document.getElementById('contact-password').value;
+  var password2 = document.getElementById('contact2-password').value;
+  if(password!==password2){
+      password2Error.innerHTML= 'Not Match'
+      return false;
+  }
+  password2Error.innerHTML= '<i class="fa-solid fa-check"></i>';
+  return true; 
+}
